9d53d18b1738918d8ae4cafbe3696eb4b2c17465
Rogier Mechielsen <rogier@swis.nl>
2018-07-25 16:25:34 +0200

    Adding fonts

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <template>
-  <div id="app">
-    <h1>Wij zijn 30 enthousiastelingen die willen werken aan jouw online succes</h1>
-    <p>Eerste alinea met tekst</p>
-    <h2>Online succes</h2>
-    <p>Online succes alinea met tekst</p>
-  </div>
+    <div id="app">
+        <typeface-loader/>
+        <h1>30 enthousiastelingen die je digitaal vooruit helpen</h1>
+        <p>Eerste alinea met tekst</p>
+        <h2>Online succes</h2>
+        <p>Online succes alinea met tekst</p>
+    </div>
 </template>
 
 <script>
-  import WebFont from 'webfontloader';
+    import TypefaceLoader from './components/TypefaceLoader';
 
-  export default {
-      name: 'app',
-      mounted() {
-          WebFont.load({
-              google: {
-                  families: ['Droid Sans', 'Droid Serif']
-              }
-          });
-      }
-  }
+    export default {
+        name: 'app',
+        components: {
+            'typeface-loader': TypefaceLoader,
+        }
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Droid Sans', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #000;
-  margin-top: 60px;
-}
+    :root {
+        --color: red;
+    }
+
+    #app {
+        font-family: var(--typeface, sans-serif);
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #000;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/TypefaceLoader.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="typeface-loader">
+        <select v-model="currentTypeface">
+            <option>sans-serif</option>
+            <optgroup label="Google Fonts">
+                <option>Droid Sans</option>
+                <option>Droid Serif</option>
+            </optgroup>
+            <optgroup label="Typekit">
+                <option>omnes-pro</option>
+                <option>azo-sans-web</option>
+                <option>nimbus-sans</option>
+                <option>halyard-display</option>
+            </optgroup>
+        </select>
+    </div>
+</template>
+
+<script>
+    import WebFont from 'webfontloader';
+
+    export default {
+        name: "TypefaceLoader",
+
+        data() {
+            return {
+                currentTypeface: 'sans-serif',
+            }
+        },
+
+        watch: {
+            currentTypeface: {
+                immediate: true,
+                handler(typeface) {
+                    document.documentElement.style.setProperty('--typeface', typeface);
+                },
+            }
+        },
+
+        mounted() {
+            WebFont.load({
+                google: {
+                    families: ['Droid Sans', 'Droid Serif']
+                },
+                typekit: {
+                    id: 'cxc7iyz',
+                }
+            });
+        },
+    }
+</script>
+
+<style lang="scss">
+
+</style>
